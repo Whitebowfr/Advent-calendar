7346bf406b003fd0d3009da1e9b6f3e091097c00
Whitebowfr <69417022+Whitebowfr@users.noreply.github.com>
2021-09-06 19:14:16 +0200

    Added the start of the part 2, not finished

--- day8.js
+++ day8.js
@@ -1,7 +1,18 @@
 var log = require("./data").days.eight
 
+log = `jmp +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6`
+
 function parseLogs() {
     let logs = []
+
     log.split("\n").forEach(line => {
         logs.push({
             type: line.match(/(acc|nop|jmp)/gi)[0],
@@ -9,6 +20,41 @@ function parseLogs() {
         })
     });
     return logs
+}
+
+function isLoopingForever(logs) {
+    let iValues = []
+    for(let i = 0; i < logs.length; i++) {
+        if (iValues.filter(o => o === i).length == 0) {
+            if (logs[i].type == "jmp") {
+                if (logs[i].value - 1 != -1) i += logs[i].value - 1
+            }
+            iValues.push(i)
+        } else {
+            return true
+        }
+    }
+    return false
+}
+function modify() {
+    let iterator = 0
+    let logss = parseLogs()
+    let ogLogs = JSON.parse(JSON.stringify(logss))
+    let lastModified = -1
+    
+    while (isLoopingForever(logss)) {
+        if (lastModified > -1) {
+            logss = JSON.parse(JSON.stringify(ogLogs))
+            console.log(logss)
+            lastModified = -1
+        }
+        if (logss[iterator].type == "nop" || logss[iterator].type == "jmp") {
+            logss[iterator].type = logss[iterator].type == "nop" ? "jmp" : "nop"
+            lastModified = iterator
+        }
+        iterator++
+    }
+
 }
 
 function getAccumulatorValue() {
@@ -33,4 +79,4 @@ function getAccumulatorValue() {
     return acc
 }
 
-console.log(getAccumulatorValue())
+modify()
